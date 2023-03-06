b4947314bec0212826ddae4721f7709dc9ccd066
DaveNganga <44644848+DaveNganga@users.noreply.github.com>
2023-03-06 15:07:38 -0500

    First Test File
    Testing Item class

--- Test/ItemTest.java
+++ Test/ItemTest.java
@@ -3,14 +3,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTest {
-    Item apple = new Item("Apple",2);
-    Item poisonApple = new Item("Poison Apple",-2);
+    Item apple = new Item("Apple", 2);
+    Item poisonApple = new Item("Poison Apple", -2);
     Item copperRing = new Item("Copper Ring", 0);
 
     @Test
-    void itemTest1(){
-        assertEquals( apple.info(), ("Item: Apple \nHealing Power: 2"));
+    void itemTest1() {
+        assertEquals(apple.info(), ("Item: Apple\nHealing Power: 2"));
     }
 
+    @Test
+    void itemTestGetters() {
+        assertEquals(poisonApple.getName(), ("Poison Apple"));
+    }
 
+    @Test
+    void itemTestGetters1() {
+        assertEquals(poisonApple.getHealingPower(), (-2));
+    }
+
+    @Test
+    void itemTestSetters() {
+        apple.setName("Pear");
+        assertEquals("Pear", apple.getName());
+    }
+
+    @Test
+    void itemTestSetters1() {
+        copperRing.setName("Useless");
+        assertEquals("Useless", copperRing.getName());
+    }
 }
